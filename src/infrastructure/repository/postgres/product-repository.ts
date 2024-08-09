999ce559279862198c22779e9ed87927037270b2
lpsgabriel <gabriel.lpst7@gmail.com>
2024-08-09 01:50:43 -0300

    refactor: separate query and params

--- src/infrastructure/repository/postgres/product-repository.ts
+++ src/infrastructure/repository/postgres/product-repository.ts
@@ -6,13 +6,14 @@ export class ProductRepositoryPostgres implements ProductRepository {
 	constructor(private readonly databaseConnection: PostgresDatabaseConnection) {}
 
 	async save(product: Product) {
-		await this.databaseConnection.query(
-			'INSERT INTO products (product_id, name, price_in_cents, description) VALUES ($1, $2, $3, $4)',
-			[product.productId, product.getName(), product.getPriceInCents(), product.getDescription()]
-		)
+		const sql = 'INSERT INTO products (product_id, name, price_in_cents, description) VALUES ($1, $2, $3, $4)'
+		const params = [product.productId, product.getName(), product.getPriceInCents(), product.getDescription()]
+		await this.databaseConnection.query(sql, params)
 	}
 
 	async delete(productId: string) {
-		await this.databaseConnection.query('DELETE FROM products WHERE product_id = $1', [productId])
+		const sql = 'DELETE FROM products WHERE product_id = $1'
+		const params = [productId]
+		await this.databaseConnection.query(sql, params)
 	}
 }
